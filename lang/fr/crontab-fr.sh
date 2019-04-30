b18de314b500d2211656cb2910b09cf8e847fee2
Poli <admin@poli.fun>
2019-04-30 11:19:49 +0200

    Update added french

---
+++ lang/fr/crontab-fr.sh
@@ -0,0 +1,85 @@
+#BDO TIMER START
+
+#Affiche tout les bosses 15 minutes avant qu'ils apparaissent
+
+0 0 * * MON /root/discord "Kutum, Karanda dans 15 minutes ( https://bdo.poli.fun/img/kutum.png ) ( https://bdo.poli.fun/img/karanda.png )"
+0 0 * * TUE /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+0 0 * * WED /root/discord "Kutum, Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kutum.png ) ( https://bdo.poli.fun/img/kzarka.png )"
+0 0 * * THU /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+0 0 * * FRI /root/discord "Kzarka, Karanda dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
+0 0 * * SAT /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+0 0 * * SUN /root/discord "Nouver,Kutum dans 15 minutes ( https://bdo.poli.fun/img/nouver.png ) ( https://bdo.poli.fun/img/kutum.png )"
+
+45 1 * * MON /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 1 * * TUE /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 1 * * WED /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 1 * * THU /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 1 * * FRI /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 1 * * SAT /root/discord "Offin dans 15 minutes ( https://bdo.poli.fun/img/offin.png )"
+45 1 * * SUN /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+
+45 4 * * MON /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 4 * * TUE /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 4 * * WED /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 4 * * THU /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 4 * * FRI /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 4 * * SAT /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 4 * * SUN /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+
+45 8 * * MON /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 8 * * TUE /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 8 * * WED /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 8 * * THU /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 8 * * FRI /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 8 * * SAT /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 8 * * SUN /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+
+45 11 * * MON /root/discord "Offin dans 15 minutes ( https://bdo.poli.fun/img/offin.png )"
+45 11 * * TUE /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 11 * * THU /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 11 * * FRI /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 11 * * SAT /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 11 * * SUN /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+
+45 15 * * MON /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 15 * * TUE /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 15 * * WED /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 15 * * THU /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 15 * * FRI /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 15 * * SAT /root/discord "Quuint/Muraka dans 15 minutes ( https://bdo.poli.fun/img/quuint.png )"
+45 15 * * SUN /root/discord "Vell dans 15 minutes ( https://bdo.poli.fun/img/vell.png )"
+
+45 18 * * MON /root/discord "Nouver dans 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 18 * * TUE /root/discord "Karanda dans 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 18 * * WED /root/discord "Offin dans 15 minutes ( https://bdo.poli.fun/img/offin.png )"
+45 18 * * THU /root/discord "Kutum dans 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 18 * * FRI /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 18 * * SAT /root/discord "Kzarka, Karanda dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
+45 18 * * SUN /root/discord "Garmoth dans 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
+
+0 22 * * MON /root/discord "Kzarka dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+0 22 * * TUE /root/discord "Garmoth dans 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
+0 22 * * WED /root/discord "Kzarka, Karanda dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
+0 23 * * WED /root/discord "Quuint/Muraka dans 15 minutes ( https://bdo.poli.fun/img/quuint.png )"
+0 22 * * THU /root/discord "Garmoth dans 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
+0 22 * * FRI /root/discord "Kzarka, Kutum dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/kutum.png )"
+0 22 * * SUN /root/discord "Kzarka, Nouver dans 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/nouver.png )"
+
+#A minuit informe sur les bosses a venir
+
+0 0 * * MON /root/discord "Boss a venir aujourd'hui : Kutum (0:15), Karanda (0:15), Karanda (2:00), Kzarka (5:00), Kzarka (9:00), Offin (12:00), Kutum (16:00), Nouver (19:00), Kzarka (22:15)"
+
+0 0 * * TUE /root/discord "Boss a venir aujourd'hui : Karanda (0:15), Kutum (2:00), Kzarka (5:00), Nouver (9:00), Kutum (12:00), Nouver (16:00), Karanda (19:00), Garmoth (22:15)"
+
+0 0 * * WED /root/discord "Boss a venir aujourd'hui : Kzarka (0:15), Kutum (2:00), Kzarka (5:00), Karanda (9:00), Kutum (16:00), Offin (19:00), Karanda (22:15), Kzarka (22:15), Quint/Muraka (23:15)"
+
+0 0 * * THU /root/discord "Boss a venir aujourd'hui : Nouver (0:15), Kutum (2:00), Nouver (5:00), Kutum (9:00), Nouver (12:00), Kzarka (16:00), Kutum (19:00), Garmoth (22:15)"
+
+0 0 * * FRI /root/discord "Boss a venir aujourd'hui : Kzarka (0:15), Karanda (0:15), Nouver (2:00), Karanda (5:00), Kutum (9:00), Karanda (12:00), Nouver (16:00), Kzarka (19:00), Kutum (22:15), Kzarka (22:15)"
+
+0 0 * * SAT /root/discord "Boss a venir aujourd'hui : Karanda (0:15), Offin (2:00), Nouver (5:00), Kutum (9:00), Nouver (12:00), Quint/Muraka (16:00), Kzarka (19:00), Karanda (19:00)"
+
+0 0 * * SUN /root/discord "Boss a venir aujourd'hui : Nouver (0:15), Kutum (0:15), Kzarka (2:00), Kutum (5:00), Nouver (9:00), Kzarka (12:00), Vell (16:00), Garmoth (19:00), Kzarka (22:15), Nouver (22:15)"
+
+
+
