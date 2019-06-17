a2addc87683dc89b359e2b3bd0bee1d1b00c6931
Poli <admin@poli.fun>
2019-06-17 15:53:27 +0200

    Update NA

--- crontab- NA.sh
+++ crontab- NA.sh
@@ -1,112 +1,104 @@
 #BDO TIMER START
 
-45 8 * * * MON /root/discord
-45 8 * * * TUE /root/discord
-45 8 * * * WED /root/discord
-45 8 * * * THU /root/discord
-45 8 * * * FRI /root/discord
-45 8 * * * SAT /root/discord
-45 8 * * * SUN /root/discord 
-
 #15 minutes before midnight PDT
 
-45 8 * * * MON /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-45 8 * * * TUE /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 8 * * * WED /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-45 8 * * * THU /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 8 * * * FRI /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 8 * * * SAT /root/discord "Offin in 15 minutes ( https://bdo.poli.fun/img/offin.png )"
-45 8 * * * SUN /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 8 * * * MON /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 8 * * * TUE /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 8 * * * WED /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 8 * * * THU /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 8 * * * FRI /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 8 * * * SAT /root/runnedscripts/bdobot/Nymph "Offin in 15 minutes ( https://bdo.poli.fun/img/offin.png )"
+45 8 * * * SUN /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
 
 #2:45 PDT
 
-45 11 * * * MON /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
-45 11 * * * TUE /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
-45 11 * * * THU /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
-45 11 * * * FRI /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-45 11 * * * SAT /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 11 * * * SUN /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 11 * * * MON /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 11 * * * TUE /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 11 * * * THU /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 11 * * * FRI /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 11 * * * SAT /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 11 * * * SUN /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
 
 #6:45 PDT
 
-45 15 * * * MON /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
-45 15 * * * TUE /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 15 * * * WED /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-45 15 * * * THU /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 15 * * * FRI /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 15 * * * SAT /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 15 * * * SUN /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 15 * * * MON /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 15 * * * TUE /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 15 * * * WED /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 15 * * * THU /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 15 * * * FRI /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 15 * * * SAT /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 15 * * * SUN /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
 
 #9:45 PDT
 
-45 18 * * * MON /root/discord "Offin in 15 minutes ( https://bdo.poli.fun/img/offin.png )"
-45 18 * * * TUE /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 18 * * * WED /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 18 * * * THU /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 18 * * * FRI /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-45 18 * * * SAT /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 18 * * * SUN /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 18 * * * MON /root/runnedscripts/bdobot/Nymph "Offin in 15 minutes ( https://bdo.poli.fun/img/offin.png )"
+45 18 * * * TUE /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 18 * * * WED /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 18 * * * THU /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 18 * * * FRI /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 18 * * * SAT /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 18 * * * SUN /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
 
 #13:45 PDT
 
-45 22 * * * MON /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 22 * * * TUE /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 22 * * * WED /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 22 * * * THU /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
-45 22 * * * FRI /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 22 * * * SAT /root/discord "Quint/Muraka in 15 minutes ( https://bdo.poli.fun/img/Quint.png )"
-45 22 * * * SUN /root/discord "Vell in 15 minutes ( https://bdo.poli.fun/img/vell.png )"
+45 22 * * * MON /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 22 * * * TUE /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 22 * * * WED /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 22 * * * THU /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 22 * * * FRI /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 22 * * * SAT /root/runnedscripts/bdobot/Nymph "Quint/Muraka in 15 minutes ( https://bdo.poli.fun/img/Quint.png )"
+45 22 * * * SUN /root/runnedscripts/bdobot/Nymph "Vell in 15 minutes ( https://bdo.poli.fun/img/vell.png )"
 
 #16:45 PDT
 
-45 1 * * * MON /root/discord "Garmoth in 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
-45 1 * * * TUE /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-45 1 * * * WED /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-45 1 * * * THU /root/discord "Offin in 15 minutes ( https://bdo.poli.fun/img/offin.png )"
-45 1 * * * FRI /root/discord "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
-45 1 * * * SAT /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
-45 1 * * * SUN /root/discord "Kzarka, Karanda in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
+45 1 * * * MON /root/runnedscripts/bdobot/Nymph "Garmoth in 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
+45 1 * * * TUE /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+45 1 * * * WED /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+45 1 * * * THU /root/runnedscripts/bdobot/Nymph "Offin in 15 minutes ( https://bdo.poli.fun/img/offin.png )"
+45 1 * * * FRI /root/runnedscripts/bdobot/Nymph "Kutum in 15 minutes ( https://bdo.poli.fun/img/kutum.png )"
+45 1 * * * SAT /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+45 1 * * * SUN /root/runnedscripts/bdobot/Nymph "Kzarka, Karanda in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
 
 #20:00 PDT
 
-0 5 * * * MON /root/discord "Kzarka, Nouver in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/nouver.png )"
-0 5 * * * TUE /root/discord "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
-0 5 * * * WED /root/discord "Garmoth in 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
-0 5 * * * THU /root/discord "Kzarka, Karanda in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
-0 5 * * * FRI /root/discord "Garmoth in 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
-0 5 * * * SAT /root/discord "Kzarka, Kutum in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/kutum.png )"
+0 5 * * * MON /root/runnedscripts/bdobot/Nymph "Kzarka, Nouver in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/nouver.png )"
+0 5 * * * TUE /root/runnedscripts/bdobot/Nymph "Kzarka in 15 minutes ( https://bdo.poli.fun/img/kzarka.png )"
+0 5 * * * WED /root/runnedscripts/bdobot/Nymph "Garmoth in 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
+0 5 * * * THU /root/runnedscripts/bdobot/Nymph "Kzarka, Karanda in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
+0 5 * * * FRI /root/runnedscripts/bdobot/Nymph "Garmoth in 15 minutes ( https://bdo.poli.fun/img/garmoth.png )"
+0 5 * * * SAT /root/runnedscripts/bdobot/Nymph "Kzarka, Kutum in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/kutum.png )"
 
 #21:15 PDT
 
-0 6 * * * THU /root/discord "Quint/Muraka in 15 minutes ( https://bdo.poli.fun/img/Quint.png )"
+0 6 * * * THU /root/runnedscripts/bdobot/Nymph "Quint/Muraka in 15 minutes ( https://bdo.poli.fun/img/Quint.png )"
 
 #22:00 PDT
 
-15 7 * * * MON /root/discord "Kutum, Karanda in 15 minutes ( https://bdo.poli.fun/img/kutum.png ) ( https://bdo.poli.fun/img/karanda.png )"
-15 7 * * * TUE /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-15 7 * * * WED /root/discord "Kzarka, Kutum in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/kutum.png )"
-15 7 * * * THU /root/discord "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
-15 7 * * * FRI /root/discord "Kzarka, Karanda in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
-15 7 * * * SAT /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
-15 7 * * * SUN /root/discord "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+15 7 * * * MON /root/runnedscripts/bdobot/Nymph "Kutum, Karanda in 15 minutes ( https://bdo.poli.fun/img/kutum.png ) ( https://bdo.poli.fun/img/karanda.png )"
+15 7 * * * TUE /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+15 7 * * * WED /root/runnedscripts/bdobot/Nymph "Kzarka, Kutum in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/kutum.png )"
+15 7 * * * THU /root/runnedscripts/bdobot/Nymph "Nouver in 15 minutes ( https://bdo.poli.fun/img/nouver.png )"
+15 7 * * * FRI /root/runnedscripts/bdobot/Nymph "Kzarka, Karanda in 15 minutes ( https://bdo.poli.fun/img/kzarka.png ) ( https://bdo.poli.fun/img/karanda.png )"
+15 7 * * * SAT /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
+15 7 * * * SUN /root/runnedscripts/bdobot/Nymph "Karanda in 15 minutes ( https://bdo.poli.fun/img/karanda.png )"
 
 
 #END First part
 #Every mid night informs about in coming bosses
 
-0 9 * * MON /root/discord "Upcoming bosses today : Karanda (right now), Kzarka (3:00), Kzarka (7:00), Offin (10:00), Kutum (14:00), Nouver (17:00), Kzarka (20:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
+0 9 * * MON /root/runnedscripts/bdobot/Nymph "Upcoming bosses today : Karanda (right now), Kzarka (3:00), Kzarka (7:00), Offin (10:00), Kutum (14:00), Nouver (17:00), Kzarka (20:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
 
-0 9 * * TUE /root/discord "Upcoming bosses today : Kutum (right now), Kzarka (3:00), Nouver (7:00), Kutum (10:00), Nouver (14:00), Karanda (17:00), Garmoth (20:15), Kutum (22:15), Kzarka (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
+0 9 * * TUE /root/runnedscripts/bdobot/Nymph "Upcoming bosses today : Kutum (right now), Kzarka (3:00), Nouver (7:00), Kutum (10:00), Nouver (14:00), Karanda (17:00), Garmoth (20:15), Kutum (22:15), Kzarka (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
 
-0 9 * * WED /root/discord "Upcoming bosses today : Karanda (right now), Karanda (7:00), Nouver (10:00), Kutum (14:00), Offin (17:00), Karanda (20:15), Kzarka (20:15), Quint/Muraka (21:15), Nouver (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
+0 9 * * WED /root/runnedscripts/bdobot/Nymph "Upcoming bosses today : Karanda (right now), Karanda (7:00), Nouver (10:00), Kutum (14:00), Offin (17:00), Karanda (20:15), Kzarka (20:15), Quint/Muraka (21:15), Nouver (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
 
-0 9 * * THU /root/discord "Upcoming bosses today : Kutum (right now), Kzarka (3:00), Kutum (7:00), Nouver (10:00), Kzarka (14:00), Kutum (17:00), Garmoth (20:15), Kzarka (22:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
+0 9 * * THU /root/runnedscripts/bdobot/Nymph "Upcoming bosses today : Kutum (right now), Kzarka (3:00), Kutum (7:00), Nouver (10:00), Kzarka (14:00), Kutum (17:00), Garmoth (20:15), Kzarka (22:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
 
-0 9 * * FRI /root/discord "Upcoming bosses today : Nouver (right now), Karanda (3:00), Kutum (7:00), Karanda (10:00), Nouver (14:00), Kzarka (17:00), Kutum (20:15), Kzarka (20:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
+0 9 * * FRI /root/runnedscripts/bdobot/Nymph "Upcoming bosses today : Nouver (right now), Karanda (3:00), Kutum (7:00), Karanda (10:00), Nouver (14:00), Kzarka (17:00), Kutum (20:15), Kzarka (20:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
 
-0 9 * * SAT /root/discord "Upcoming bosses today : Offin (right now), Nouver (3:00), Kutum (7:00), Nouver (10:00), Quint/Muraka (14:00), Kzarka (17:00), Karanda (17:00), Nouver (22:15), Kutum (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
+0 9 * * SAT /root/runnedscripts/bdobot/Nymph "Upcoming bosses today : Offin (right now), Nouver (3:00), Kutum (7:00), Nouver (10:00), Quint/Muraka (14:00), Kzarka (17:00), Karanda (17:00), Nouver (22:15), Kutum (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
 
-0 9 * * SUN /root/discord "Upcoming bosses today : Kzarka (right now), Kutum (3:00), Kzarka (10:00), Vell (14:00), Garmoth (17:00), Kzarka (20:15), Nouver (20:15), Kutum (22:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
+0 9 * * SUN /root/runnedscripts/bdobot/Nymph "Upcoming bosses today : Kzarka (right now), Kutum (3:00), Kzarka (10:00), Vell (14:00), Garmoth (17:00), Kzarka (20:15), Nouver (20:15), Kutum (22:15), Karanda (22:15)\n Created by Poli#9036 with the lov of lolis and :unicorn:"
 
 
 #BDO TIMER STOP
