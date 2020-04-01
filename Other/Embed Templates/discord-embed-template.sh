a8d15cd4b42ff3c466b7a166f2bc86a6c2f5d8bc
Poli <admin@polisystems.ch>
2020-04-01 13:41:04 +0200

    udp

---
+++ Other/Embed Templates/discord-embed-template.sh
@@ -0,0 +1,105 @@
+## For double image
+
+{
+  "embeds": [
+    {
+      "color": 13632027,
+      "title": "Kzarka, Nouver dans 15 minutes",
+      "description": "Exactement à 22:15",
+      "image": {
+        "url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/kzarka.png"
+      },
+      "author": {
+        "name": "Black Desert Online Boss Timer",
+        "icon_url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/blackspirit.png"
+      }
+    },
+    {
+      "color": 14513011,
+      "footer": {
+        "icon_url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/polisystems.png",
+        "text": "Sponsored by Poli Systems (https://polisystems.ch) "
+      },
+      "image": {
+        "url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/nouver.png"
+      }
+    }
+  ]
+}
+
+## For one image
+
+{
+  "embeds": [
+    {
+      "title": "Garmoth dans 15 minutes",
+      "description": "Exactement à 19:00",
+      "color": 16712965,
+      "footer": {
+        "icon_url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/polisystems.png",
+        "text": "Sponsored by Poli Systems (https://polisystems.ch) "
+      },
+      "image": {
+        "url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/garmoth.png"
+      },
+      "author": {
+        "name": "Black Desert Online Boss Timer",
+        "icon_url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/blackspirit.png"
+      }
+    }
+  ]
+}
+
+## For the list of boss
+
+{
+  "embeds": [
+    {
+      "title": "Liste de boss a venir aujourd'hui",
+      "description": "----------",
+      "color": 8716032,
+      "footer": {
+        "icon_url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/polisystems.png",
+        "text": "Sponsored by Poli Systems (https://polisystems.ch) "
+      },
+      "author": {
+        "name": "Black Desert Online Boss Timer",
+        "icon_url": "https://raw.githubusercontent.com/IIPoliII/BDO-Easy-Boss-Timer/master/img/blackspirit.png"
+      },
+      "fields": [
+        {
+          "name": "00:15",
+          "value": "Kutum, Kranda"
+        },
+        {
+          "name": "2:00",
+          "value": "Karanda"
+        },
+        {
+          "name": "5:00",
+          "value": "Kzarka"
+        },
+        {
+          "name": "9:00",
+          "value": "Kzarka"
+        },
+        {
+          "name": "12:00",
+          "value": "Offin"
+        },
+        {
+          "name": "16:00",
+          "value": "Kutum"
+        },
+        {
+          "name": "19:00",
+          "value": "Nouver"
+        },
+        {
+          "name": "22:15",
+          "value": "Kzarka"
+        }
+      ]
+    }
+  ]
+}
